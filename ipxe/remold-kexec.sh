728972c5c08be2077215ef3a9aa6d653001355a0
Jordan Sissel <jls@semicomplete.com>
2026-09-06 18:51:24 -0700

    WIP trying to make kexec work for this purpose...
    But on arm64/aarch64 different distros use different "types" (?) for
    kernel images, and attempting to kexec a Debian 13 kernel from a Fedora
    44 host will cause the new kernel to get started and then immediately
    stall and do nothing else forever, best I can tell...

--- ipxe/remold-kexec.sh
+++ ipxe/remold-kexec.sh
@@ -3,17 +3,34 @@
 set -e 
 
 sudo dnf install -y kexec-tools
+#sudo apt-get install -y kexec-tools
 
 install_params="auto=true priority=critical preseed/url=http://10.0.2.2:29145/installer/debian-preseed.txt"
 mirror=http://10.0.2.2:29145
 base_dir=debian
 debian_version=trixie
-arch=amd64
+arch=arm64
 mirrorcfg=mirror/suite=${debian_version}
-dir="${mirror}/${base_dir}/dists/${debian_version}/main/installer-${arch}/current/images/netboot/debian-installer/amd64/"
+dir="${mirror}/${base_dir}/dists/${debian_version}/main/installer-${arch}/current/images/netboot/debian-installer/${arch}/"
 
 cd /tmp
-curl -LO "$dir/linux"
-curl -LO "$dir/initrd.gz"
+curl -Lo "linux" "$dir/linux"
+curl -Lo "initrd.gz" "$dir/initrd.gz"
 
-sudo kexec /tmp/linux --initrd=/tmp/initrd.gz --append="${install_params} ${mirrorcfg} -- quiet initrd=initrd.gz"
+#sudo kexec /boot/vmlinuz-$(uname -r) --initrd=/tmp/initrd.gz --append="${install_params} ${mirrorcfg} --- console=/dev/tty1 console=/dev/ttyAMA0,115200n8 initrd=initrd.gz"
+sudo kexec --help
+#set -x
+
+#sudo kexec --type=uki --initrd=/tmp/initrd.gz --append="${install_params} ${mirrorcfg} --- console=/dev/tty1 console=/dev/ttyAMA0,115200n8 initrd=initrd.gz" linux
+
+#for i in vmlinux vmlinuz Image uki uImage ; do
+  #echo ">>> $i"
+  #sudo kexec -l --type=$i --command-line="console=/dev/tty1 console=/dev/ttyAMA0,115200n8 initrd=initrd.gz" linux
+#done
+sudo kexec -l --type=Image linux
+sudo kexec -e
+#--append="console=/dev/tty1 console=/dev/ttyAMA0,115200n8 initrd=initrd.gz" linux
+#sudo kexec linux --type=vmlinuz --initrd=/tmp/initrd.gz --append="${install_params} ${mirrorcfg} --- console=/dev/tty1 console=/dev/ttyAMA0,115200n8 initrd=initrd.gz"
+#sudo kexec linux --type=vmlinux --initrd=/tmp/initrd.gz --append="${install_params} ${mirrorcfg} --- console=/dev/tty1 console=/dev/ttyAMA0,115200n8 initrd=initrd.gz"
+#sudo kexec linux --type=Image --initrd=/tmp/initrd.gz --append="${install_params} ${mirrorcfg} --- console=/dev/tty1 console=/dev/ttyAMA0,115200n8 initrd=initrd.gz"
+#--append="${install_params} ${mirrorcfg} --- console=/dev/tty1 console=/dev/ttyAMA0,115200n8 initrd=initrd.gz"
