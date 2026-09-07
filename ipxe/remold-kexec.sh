da4d393fb4c2f485ad2c9efaa4f3bb2d3afd9f32
Jordan Sissel <jls@semicomplete.com>
2026-09-06 23:14:49 -0700

    Try a real machine to reimage via kexec

--- ipxe/remold-kexec.sh
+++ ipxe/remold-kexec.sh
@@ -5,8 +5,11 @@ set -e
 sudo dnf install -y kexec-tools
 #sudo apt-get install -y kexec-tools
 
+dlhost="$(avahi-resolve --name mgmt.local | cut -f 2)"
+#dlhost="10.0.2.2"
+
 arch="$(uname -m)"
-mirror="http://10.0.2.2:29145/fedora/linux/releases/44"
+mirror="http://${dlhost}:29145/fedora/linux/releases/44"
 repo="${mirror}/Everything/${arch}/os"
 
 cd /tmp
@@ -18,5 +21,7 @@ set -x
 
 sudo kexec \
 	--initrd initrd.img \
-	--append "console=tty1 console=ttyS0,115200n8 inst.repo=${repo} inst.ks=http://10.0.2.2:29145/installer/fedora.ks" \
+	--append "console=tty1 console=ttyS0,115200n8 inst.repo=${repo} inst.ks=http://${dlhost}:29145/installer/fedora.ks" \
 	vmlinuz
+
+	#--append "console=tty1 console=ttyS0,115200n8 inst.repo=${repo} inst.ks=http://${dlhost}:29145/installer/fedora.ks" \
