6bfeafa206bd93d5525c69facaccea76d97dfb50
Jordan Sissel <jls@semicomplete.com>
2026-09-06 18:52:42 -0700

    More progress now attempting on arm64/aarch64.
    Found a hopefully-more-correct partition number lookup.
    
    Added comments to what the efibootmgr commands are doing.

--- ipxe/remold.sh
+++ ipxe/remold.sh
@@ -2,8 +2,10 @@
 
 set -e 
 
-sudo dnf install -y ipxe-bootimgs-x86 jq
-sudo cp /usr/share/ipxe/ipxe-x86_64.efi /boot/efi/EFI
+#sudo dnf install -y ipxe-bootimgs-x86 jq
+#sudo cp /usr/share/ipxe/ipxe-x86_64.efi /boot/efi/EFI
+
+#sudo apt-get install -y ipxe jq efibootmgr
 
 _jq() {
 	jq --arg efiroot /boot/efi "$@"
@@ -20,7 +22,10 @@ dev="$(echo "$devinfo" | jq -r .name)"
 # If it's not found, index() returns null
 # Also, the index, if found, is zero offset. So, add 1 to make it a partition number.
 # Null becomes zero, in this case, indicating an error finding the /boot/efi device partiition.
-partition="$(echo "$devinfo" | _jq '.children | map(.mountpoints | contains([$efiroot])) | (index(true) // -1) + 1')"
+partition="$(echo "$devinfo" | _jq '.children | map(.mountpoints | contains([$efiroot])) | (index(true) // 0) + 1')"
+
+# Grab the final numbers of the mount device, like vda15 => 15, or nvme0n1p3 -> 3
+partdev="$(echo "$devinfo" | _jq -r '.children[] | select (.mountpoints | contains([$efiroot])) | .name' | grep -Eo '[0-9]+$')"
 
 if [ "$partition" -eq 0 ] ; then
 	echo "Couldn't find device with /boot/efi mounted?"
@@ -31,17 +36,20 @@ echo "Device: $dev"
 echo "Partition: $partition"
 
 # Generate autoexec.ipxe
-sudo tee /boot/efi/EFI/autoexec.ipxe << AUTOEXEC
+sudo tee /boot/efi/EFI/autoexec.ipxe > /dev/null << AUTOEXEC
 #!ipxe
 
 echo Hello world
 sleep 1
 
 # Set repository URI
+set arch aarch64
 set mirror http://10.0.2.2:29145/fedora/linux/releases/44
-set repo \${mirror}/Everything/x86_64/os
+set repo \${mirror}/Everything/\${arch}/os
 
 dhcp
+
+echo 
 
 # Start installer
 kernel \${repo}/images/pxeboot/vmlinuz inst.repo=\${repo}
@@ -51,12 +59,19 @@ AUTOEXEC
 
 # Install ipxe and set nextboot to use it.
 
-sudo cp /usr/share/ipxe/ipxe-x86_64.efi /boot/efi/EFI
+arch="arm64"
+#sudo cp /usr/share/ipxe/ipxe-x86_64.efi /boot/efi/EFI
+sudo cp /usr/lib/ipxe/ipxe-${arch}.efi /boot/efi/EFI
+echo "OK"
 
 # Delete any old entry
 sudo efibootmgr -B -L ipxe || true
 
-sudo efibootmgr -c -l "\\EFI\\ipxe-x86_64.efi" -L ipxe -d "/dev/$dev" -p "$partition"
-sudo efibootmgr -n $(sudo efibootmgr  | sed -Ene '/ipxe-x86_64.efi/s/^Boot([0-9A-Fa-f]+).*/\1/p')
+# Create new entry
+sudo efibootmgr -c -l "\\EFI\\ipxe-${arch}.efi" -L ipxe -d "/dev/$dev" -p "$partdev"
 
+# Set it as next boot
+sudo efibootmgr -n $(sudo efibootmgr  | sed -Ene "/ipxe-${arch}.efi/s/^Boot([0-9A-Fa-f]+).*/\1/p")
+
+# Let's gooooooo
 sudo reboot
